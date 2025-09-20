8e32109aabfcd82e074aab8f438feb558455345c
ASDBUDDY\adira <as3455@student.aru.ac.uk>
2025-09-20 09:06:49 +0100

    Wall Jump fixes + Gravity functionality

--- Assets/Scripts/PlayerScripts/PlayerMainScript.cs
+++ Assets/Scripts/PlayerScripts/PlayerMainScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -20,26 +21,38 @@ public class PlayerMainScript : MonoBehaviour
     [SerializeField]
     private Vector2 wallCheckSize = new Vector2(0.58f, 0.12f);
     public LayerMask WallLayerMask;
-    private bool isWallSliding = false;
 
+    [Header("Gravity Params")]
+    public float BaseGravity;
+    public float GravityMultiplier;
 
     private Rigidbody2D playerRigidbody;
     private SpriteRenderer playerSpriteRenderer;
     private PlayerAnimatorScript playerAnimatorScript;
 
         #region Movement variables
+        //Movement
         private float horizontalMovement = 0f;
         private float movementSmoothTime = 0.3f;
         private Vector2 velocityRef = Vector2.zero;
 
+        //Jump variables
         private float timeLastPressedJump = 0f;
         private float timeSinceLastJump = 0f;
         private float lastTimeOnGround = 0f;
         private float coyoteTime = 0.2f;
         private float jumpBufferInterval = 0.1f; 
         private bool isHalfJump = false;
+        private bool isWallJumping = false;
+    [SerializeField]
+    private float wallJumpTimer = 0f;
+
+    //Sliding variabls
+    private bool isWallSliding = false;
+    private float wallSlidingTimer = 0f;
+    private float wallSlidingCooldown =0.5f;
     #endregion
-   
+
     #endregion
 
     #region MonoBehaviour Methods
@@ -92,68 +105,136 @@ public class PlayerMainScript : MonoBehaviour
     {
         if (playerRigidbody != null)
         {
-            if (horizontalMovement < 0f)
-            {
-                transform.rotation = Quaternion.Euler(transform.position.x, 180, transform.position.z);
-
-            }
-            else if (horizontalMovement > 0f) 
-            {
-                transform.rotation = Quaternion.Euler(transform.position.x, 0, transform.position.z);
-            }
-                playerRigidbody.velocity = Vector2.SmoothDamp(playerRigidbody.velocity, new Vector2(horizontalMovement * MainStats.PlayerSpeed, playerRigidbody.velocity.y), ref velocityRef, movementSmoothTime);
             
-            playerAnimatorScript.SetVelocity(Mathf.Clamp01(Mathf.Abs(playerRigidbody.velocity.x)), playerRigidbody.velocity.y);
+                if (horizontalMovement < 0f)
+                {
+                    transform.rotation = Quaternion.Euler(transform.position.x, 180, transform.position.z);
+
+                }
+                else if (horizontalMovement > 0f)
+                {
+                    transform.rotation = Quaternion.Euler(transform.position.x, 0, transform.position.z);
+                }
+                playerRigidbody.velocity = Vector2.SmoothDamp(playerRigidbody.velocity, new Vector2(horizontalMovement * MainStats.PlayerSpeed *(IsGrounded() ? 1f:0.8f), playerRigidbody.velocity.y), ref velocityRef, movementSmoothTime * (isWallJumping ? 0.5f:1f ));
+
+                playerAnimatorScript.SetVelocity(Mathf.Clamp01(Mathf.Abs(playerRigidbody.velocity.x)), playerRigidbody.velocity.y);
+            
 
             GroundCheck();
             ProcessWallSlide();
+            ProcessWallJump();
             if(IsGrounded() && Time.time - timeLastPressedJump <= jumpBufferInterval)
             {
                 
                 PerformJump(isHalfJump);
             }
-
+            GravityFunctionality();
            
         }
     }
 
+    private void GravityFunctionality()
+    {
+        if(playerRigidbody != null)
+        {
+            if(playerRigidbody.velocity.y < 0f && !isWallSliding)
+            {
+                playerRigidbody.gravityScale = BaseGravity * GravityMultiplier;
+                playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, Mathf.Max(playerRigidbody.velocity.y, -MainStats.PlayerMaxFallSpeed));
+            }
+            else
+            {
+                playerRigidbody.gravityScale = BaseGravity;
+            }
+        }
+    }
     private void ProcessWallSlide()
     {
-        if (!IsGrounded() && IsOnWall() && horizontalMovement != 0)
+
+        if (!IsGrounded() && IsOnWall() && horizontalMovement!=0 && wallSlidingTimer <= 0f)
         {
             isWallSliding = true;
             playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, Mathf.Max(playerRigidbody.velocity.y, -MainStats.PlayerWallSlideSpeed));
+            
         }
         else
         {
+            if (isWallSliding)
+            {
+                wallSlidingTimer = wallSlidingCooldown;
+            }
             isWallSliding = false;
         }
 
+        if(wallSlidingTimer > 0f) 
+            wallSlidingTimer -= Time.deltaTime;
+
         playerAnimatorScript.SetWallSide(isWallSliding);
+    }
+    private void ProcessWallJump()
+    {
+        if (isWallSliding || IsOnWall() && !isWallJumping)
+        {
+            isWallJumping = false;
+            wallJumpTimer = MainStats.PlayerWallJumpTime;
+
+            CancelInvoke(nameof(CancelWallJump));
+        }
+        else if (wallJumpTimer > 0f)
+        {
+            wallJumpTimer -= Time.deltaTime;
+            
+        }
+    }
+    private void CancelWallJump()
+    {
+        isWallJumping = false;
     }
     private void GroundCheck()
     {
         if (IsGrounded())
         {
             lastTimeOnGround = Time.time;
-            if(playerRigidbody.velocity.y <= 0f)
+            if(playerRigidbody.velocity.y <= 0.1f)
                 playerAnimatorScript.SetJump(false);
         }
     }
 
     public void JumpFunction(InputAction.CallbackContext context)
     {
-        timeLastPressedJump = Time.time;
-        isHalfJump = context.performed ? false : context.canceled ? true : false;
 
-        if (IsGrounded() || Time.time - lastTimeOnGround <= coyoteTime)
+         timeLastPressedJump = Time.time;
+         isHalfJump = context.performed ? false : context.canceled ? true : false;
+
+        if (Time.time - timeSinceLastJump > 0.5f)
         {
+            if (IsGrounded() || Time.time - lastTimeOnGround <= coyoteTime)
+            {
 
-            PerformJump(isHalfJump);
-            
+                PerformJump(isHalfJump);
+                
+            }
+
+            if (context.performed && wallJumpTimer > 0f)
+            {
+                
+
+                isWallJumping = true;
+                float direction = transform.rotation.eulerAngles.y == 180f ? 1 : -1;
+               
+                playerRigidbody.velocity = new Vector2(MainStats.PlayerWallJumpSpeed * direction, MainStats.PlayerJumpSpeed);
+                transform.rotation = Quaternion.Euler(transform.position.x, direction == 1 ? 0 : 180, transform.position.z);
+                wallJumpTimer = 0f;
+
+
+                playerAnimatorScript.SetJump(true);
+                Invoke(nameof(CancelWallJump), MainStats.PlayerWallJumpTime + 0.1f);
+                timeSinceLastJump = Time.time;
+            }
         }
     }
 
+   
     private void PerformJump(bool isHalf = false)
     {
         playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, MainStats.PlayerJumpSpeed * (isHalf ? 0.5f : 1f));
@@ -178,5 +259,8 @@ public class PlayerStats
     public float PlayerSpeed;
     public float PlayerJumpSpeed;
     public float PlayerWallSlideSpeed;
+    public float PlayerWallJumpTime;
+    public float PlayerWallJumpSpeed;
+    public float PlayerMaxFallSpeed;
 
 }
