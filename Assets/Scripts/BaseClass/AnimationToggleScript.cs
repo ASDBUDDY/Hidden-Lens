36bab256bc170d63ec28d6be8b7d4f72749f86e1
ASDBUDDY\adira <as3455@student.aru.ac.uk>
2025-10-16 17:37:10 +0100

    Swinging Obstacles added

--- Assets/Scripts/BaseClass/AnimationToggleScript.cs
+++ Assets/Scripts/BaseClass/AnimationToggleScript.cs
@@ -4,7 +4,21 @@ using UnityEngine;
 
 public class AnimationToggleScript : MonoBehaviour
 {
-   
+    public bool ToggleSpeed = false;
+    public float AnimSpeed = 1f;
+
+    private Animator animatorObj;
+
+    private void Awake()
+    {
+        animatorObj = GetComponent<Animator>();
+    }
+    private void Start()
+    {
+        if(ToggleSpeed)
+        animatorObj.speed = AnimSpeed;
+    }
+
     public void DisableObj()
     {
         this.gameObject.SetActive(false);
